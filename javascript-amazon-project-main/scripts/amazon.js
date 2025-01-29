456276746ba49b07853c686f10137b6932689fe2
neelbuddhdev <neelbuddhdev2513@gmail.com>
2025-01-29 17:12:10 +0530

    Use products.js file

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -5,35 +5,45 @@
 
 // We will use objects to group multiple values together
 
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 1090, // caluctate in cents for better call instead of 10.90
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Composite Basketball",
-    rating: {
-      stars: 4,
-      count: 127,
-    },
-    priceCents: 2095, // calculate in cents for better call instead of 15
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt 2-Pack Teal",
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceCents: 799, // calculate in cents for better call instead of 12
-  },
-];
+// const products = [
+//   {
+//     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+//     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+//     rating: {
+//       stars: 4.5,
+//       count: 87,
+//     },
+//     priceCents: 1090, // caluctate in cents for better call instead of 10.90
+//   },
+//   {
+//     image: "images/products/intermediate-composite-basketball.jpg",
+//     name: "Intermediate Composite Basketball",
+//     rating: {
+//       stars: 4,
+//       count: 127,
+//     },
+//     priceCents: 2095, // calculate in cents for better call instead of 15
+//   },
+//   {
+//     image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+//     name: "Adults Plain Cotton T-Shirt 2-Pack Teal",
+//     rating: {
+//       stars: 4.5,
+//       count: 56,
+//     },
+//     priceCents: 799, // calculate in cents for better call instead of 12
+//   },
+//   {
+//     image: "images/products/black-2-slot-toaster.jpg",
+//     name: "2 Slot Toaster - Black",
+//     rating: {
+//       stars: 5,
+//       count: 2197,
+//     },
+//     priceCents: 1899,
+//   },
+// ];
+// commented as importing from data folder
 
 // So now we will generate the inner html and for that we will use for each for our array and to avoid boiler plate/repetitive code we will add ${} in foreach and use single time html
 let productsHTML = "";
